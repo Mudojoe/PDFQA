f6ec2c2943f18fb4efc81aaa764320b1d99ce752
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2024-02-03 19:24:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+# from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+import os
+# from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+import streamlit as st
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Set the OpenAI API key
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+
+
+# vectordb = None
+# persist_dir = "MyTextEmbedding"
+# embedding = OpenAIEmbeddings()
+# vectordb = Chroma(persist_directory=persist_dir, embedding_function=embedding)
+# chain = RetrievalQA.from_chain_type(OpenAI(), retriever=vectordb.as_retriever(), chain_type="stuff")
+
+st.title('Skimlit')
+txt = st.text_area('Text to analyze', '''Ask the Question''')
+
+print("hi")
+
+# Load saved model
+vectordb = None
+persist_dir = "MyTextEmbedding"
+embedding = OpenAIEmbeddings(model="text-embedding-3-large")
+vectordb = Chroma(persist_directory=persist_dir, embedding_function=embedding)
+chain = RetrievalQA.from_chain_type(OpenAI(), retriever=vectordb.as_retriever(), chain_type="stuff")
+
+def get_results(abs_text):
+    list1 = chain.invoke(abs_text)
+    list1['result'] = list1['result'].replace('\n', '')
+    list1['result']
+
+print("\n\nAI formatted abstract is given below:\n")
+
+if st.button('Ask'):
+    get_results(txt)
