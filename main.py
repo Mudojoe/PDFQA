795f853c3d267ad39f3ef71aaf3f8791173ce9b8
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2024-02-04 00:44:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# from PyPDF2 import PdfReader
-from langchain.embeddings.openai import OpenAIEmbeddings
-# from langchain.text_splitter import CharacterTextSplitter
+from PyPDF2 import PdfReader
+from langchain.document_loaders import TextLoader,PyPDFLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
-import os
-# from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+from langchain_openai import OpenAI
 from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
+from langchain.document_loaders import PyPDFLoader
 import streamlit as st
+import os
+import chromadb
 
 # Load environment variables from .env file
 load_dotenv()
@@ -31,7 +33,7 @@ print("hi")
 # Load saved model
 vectordb = None
 persist_dir = "MyTextEmbedding"
-embedding = OpenAIEmbeddings(model="gpt-4")
+embedding = OpenAIEmbeddings(model="text-embedding-3-large")
 vectordb = Chroma(persist_directory=persist_dir, embedding_function=embedding)
 chain = RetrievalQA.from_chain_type(OpenAI(), retriever=vectordb.as_retriever(), chain_type="stuff")
 
